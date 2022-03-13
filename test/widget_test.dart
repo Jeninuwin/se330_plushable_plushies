aa228e66b0c80e9a48a087a4f0fba1282a8777bf
Jenny Nguyen <44487057+Jeninuwin@users.noreply.github.com>
2022-03-12 21:00:36 -0600

    adding images, navbar, and folders

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:se330_plushable_plushies/main.dart';
+import 'package:pp/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
