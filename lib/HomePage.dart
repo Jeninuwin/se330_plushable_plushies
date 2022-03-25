a410bd618fd5cdae60e05ce63811e3b0828b4b44
Jenny Nguyen <44487057+Jeninuwin@users.noreply.github.com>
2022-03-24 20:58:06 -0500

    navigation somewhat working

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -10,23 +12,43 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Page'),
+        title: const Text('Home Page'),
       ),
       body: Center(
-        child: Container(
-          height: 80,
-          width: 150,
-          decoration: BoxDecoration(
-              color: Colors.blue, borderRadius: BorderRadius.circular(10)),
-          child: FlatButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: Text(
-              'Welcome',
-              style: TextStyle(color: Colors.white, fontSize: 25),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(0.20),
+                  child: Image.asset('assets/images/test.png'),
+                ),
+                const Padding(padding: EdgeInsets.all(8.0)),
+                RichText(
+                    text: const TextSpan(children: [
+                  WidgetSpan(
+                    child: Icon(
+                      Icons.arrow_back,
+                      color: Colors.black,
+                      size: 20.0,
+                    ),
+                  ),
+                  TextSpan(
+                    text: 'What\'s Hot',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  WidgetSpan(
+                    child: Icon(
+                      Icons.arrow_forward,
+                      color: Colors.black,
+                      size: 20.0,
+                    ),
+                  ),
+                ]))
+              ],
             ),
-          ),
+            Row()
+          ],
         ),
       ),
     );
--- lib/login.dart
+++ lib/login.dart
@@ -2,21 +2,9 @@ import 'package:flutter/material.dart';
 
 import 'HomePage.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: LoginDemo(),
-    );
-  }
-}
-
 class LoginDemo extends StatefulWidget {
+  const LoginDemo({Key? key}) : super(key: key);
+
   @override
   _LoginDemoState createState() => _LoginDemoState();
 }
@@ -27,7 +15,7 @@ class _LoginDemoState extends State<LoginDemo> {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text("Login Page"),
+        title: const Text("Login Page"),
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -55,11 +43,10 @@ class _LoginDemoState extends State<LoginDemo> {
               ),
             ),
             const Padding(
-              padding: EdgeInsets.only(
-                  left: 15.0, right: 15.0, top: 15, bottom: 0),
+              padding:
+                  EdgeInsets.only(left: 15.0, right: 15.0, top: 15, bottom: 0),
               //padding: EdgeInsets.symmetric(horizontal: 15),
               child: TextField(
-
                 obscureText: true,
                 decoration: InputDecoration(
                     border: OutlineInputBorder(),
@@ -68,7 +55,7 @@ class _LoginDemoState extends State<LoginDemo> {
               ),
             ),
             TextButton(
-              onPressed: (){
+              onPressed: () {
                 //TODO FORGOT PASSWORD SCREEN GOES HERE
               },
               child: const Text(
@@ -86,7 +73,7 @@ class _LoginDemoState extends State<LoginDemo> {
                   Navigator.push(
                       context, MaterialPageRoute(builder: (_) => HomePage()));
                 },
-                child:  const Text(
+                child: const Text(
                   'Login',
                   style: TextStyle(color: Colors.white, fontSize: 25),
                 ),
@@ -96,9 +83,8 @@ class _LoginDemoState extends State<LoginDemo> {
               height: 110,
             ),
             const Text('New User?'),
-            
             TextButton(
-              onPressed: (){
+              onPressed: () {
                 //TODO FORGOT PASSWORD SCREEN GOES HERE
               },
               child: const Text(
