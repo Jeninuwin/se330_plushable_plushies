08858ea25478f016c73088696718bf1a56df7204
Adborchert <andrewborchert97@gmail.com>
2022-03-30 02:57:08 -0500

    Added Second row in homepage

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -16,7 +16,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
+      body: Center(
         child: Center(
           child: Column(
             children: <Widget>[
@@ -27,48 +27,113 @@ class _HomePageState extends State<HomePage> {
                     child: Image.asset('assets/images/test.png'),
                   ),
                   const Padding(padding: EdgeInsets.all(8.0)),
-                  RichText(
-                      text: const TextSpan(children: [
-                    WidgetSpan(
-                      child: Icon(
-                        Icons.arrow_back,
-                        color: Colors.black,
-                        size: 20.0,
+                  const Text(
+                    'What\'s Hot\n',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Column(
+                          children: <Widget>[
+                            Image.asset(
+                              'assets/images/Yorkie.jpg',
+                              height: 100,
+                            ),
+                            Text('Yorkie Bitch'),
+                          ],
+                        ),
                       ),
-                    ),
-                    TextSpan(
-                      text: 'What\'s Hot',
-                      style: TextStyle(color: Colors.black),
-                    ),
-                    WidgetSpan(
-                      child: Icon(
-                        Icons.arrow_forward,
-                        color: Colors.black,
-                        size: 20.0,
+                      Expanded(
+                        child: Column(
+                          children: <Widget>[
+                            Image.asset(
+                              'assets/images/Yorkie.jpg',
+                              height: 100,
+                            ),
+                            Text('Yorkie Hoe'),
+                          ],
+                        ),
                       ),
-                    ),
-                  ]))
+                      Expanded(
+                        child: Column(
+                          children: <Widget>[
+                            Image.asset(
+                              'assets/images/Yorkie.jpg',
+                              height: 100,
+                            ),
+                            Text('Yorkie Hoe'),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        child: Column(
+                          children: <Widget>[
+                            Image.asset(
+                              'assets/images/Yorkie.jpg',
+                              height: 100,
+                            ),
+                            Text('Yorkie Hoe'),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Padding(padding: EdgeInsets.all(8.0)),
+                  const Text(
+                    'What\'s Hot\n',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Column(
+                          children: <Widget>[
+                            Image.asset(
+                              'assets/images/Yorkie.jpg',
+                              height: 100,
+                            ),
+                            Text('Yorkie Bitch'),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        child: Column(
+                          children: <Widget>[
+                            Image.asset(
+                              'assets/images/Yorkie.jpg',
+                              height: 100,
+                            ),
+                            Text('Yorkie Hoe'),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        child: Column(
+                          children: <Widget>[
+                            Image.asset(
+                              'assets/images/Yorkie.jpg',
+                              height: 100,
+                            ),
+                            Text('Yorkie Hoe'),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        child: Column(
+                          children: <Widget>[
+                            Image.asset(
+                              'assets/images/Yorkie.jpg',
+                              height: 100,
+                            ),
+                            Text('Yorkie Hoe'),
+                          ],
+                        ),
+                      ),
+                    ],
+                  )
                 ],
               ),
-              Row(
-                // ignore: unnecessary_new
-                children: const <Widget>[
-                  Expanded(
-                    child: Text('Deliver features faster',
-                        textAlign: TextAlign.center),
-                  ),
-                  Expanded(
-                    child: Text('Craft beautiful UIs',
-                        textAlign: TextAlign.center),
-                  ),
-                  Expanded(
-                    child: FittedBox(
-                      fit: BoxFit.contain, // otherwise the logo will be tiny
-                      child: FlutterLogo(),
-                    ),
-                  ),
-                ],
-              )
             ],
           ),
         ),
